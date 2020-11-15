da8ed7042b79e9c3f5d3a3f9f69acc17ad054194
Antricks <mdecker@posteo.de>
2020-11-15 12:17:35 +0100

    testumgebung verbessert

--- test.sh
+++ test.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
+input=$1
+compressed="$1.compressed"
+restored="$1.restored"
+
 echo "----------------"
 echo "compress"
 echo "----------------"
-./main.py cn $1 compressed.txt
+./main.py cn $1 $compressed
 echo "----------------"
 echo "decompress"
 echo "----------------"
-./main.py dn compressed.txt restored.txt
+./main.py dn $compressed $restored
 
 echo "----------------"
 echo "diff"
 echo "----------------"
-./diff.py $1 restored.txt
+./diff.py $1 $restored
 echo "----------------"
 echo "chardiff"
 echo "----------------"
-./chardiff.py $1 restored.txt
+./chardiff.py $1 $restored
 echo "----------------"
 echo "md5sum"
 echo "----------------"
-md5sum $1 restored.txt
-
-rm restored.txt compressed.txt
+md5sum $1 $restored
