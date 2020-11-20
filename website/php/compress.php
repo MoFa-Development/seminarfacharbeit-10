bfc4f369d2cadd73136472526a9c3e09d4749bc7
SpyrexDE <malikfk100@gmail.com>
2020-11-20 16:59:51 +0100

    omg ._.

--- website/php/compress.php
+++ website/php/compress.php
@@ -6,6 +6,6 @@ $file = fopen("/tmp/temp.txt", "w") or die("Datei konnte nicht geöffnet werden.
 fwrite($file, $text) or die("Die Eingabe ist leer.");
 fclose($file) or die("Datei konnte nicht geschlossen werden.");
 
-$output = var_dump(shell_exec('py ../../main.py cn /tmp/temp.txt 2>&1'));
+$output = var_dump(shell_exec('python3 ../../main.py cn /tmp/temp.txt 2>&1'));
 
 echo $output;
