c7cb6b49b47a11f9e3a0ead2a5a7e529171f4cb3
SpyrexDE <malikfk100@gmail.com>
2020-11-20 16:53:03 +0100

    .

--- website/php/compress.php
+++ website/php/compress.php
@@ -6,6 +6,6 @@ $file = fopen("/tmp/temp.txt", "w") or die("Datei konnte nicht geöffnet werden.
 fwrite($file, $text) or die("Datei konnte nicht beschrieben werden.");
 fclose($file) or die("Datei konnte nicht geschlossen werden.");
 
-$output = shell_exec('py ../../main.py cn ../../texts/temp.txt') or die("Komprimierungsfehler");
+$output = shell_exec('py ../../main.py cn /tmp/temp.txt') or die("Komprimierungsfehler");
 
 echo $output;
