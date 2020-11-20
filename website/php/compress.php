6247ad2805d316aa5bdfd7c4895f10c5b1c4e003
SpyrexDE <malikfk100@gmail.com>
2020-11-20 17:11:51 +0100

    .

--- website/php/compress.php
+++ website/php/compress.php
@@ -5,6 +5,7 @@ $text = $_POST["text"];
 $file = fopen("/tmp/temp.txt", "w") or die("Datei konnte nicht geöffnet werden.");
 fwrite($file, $text) or die("Die Eingabe ist leer.");
 fclose($file) or die("Datei konnte nicht geschlossen werden.");
+unlink($file_pointer);
 
 $output = shell_exec('python3 ../../main.py cn /tmp/temp.txt') or die("Komprimierungsfehler");
 
