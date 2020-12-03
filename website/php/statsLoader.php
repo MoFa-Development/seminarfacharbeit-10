874a015833385415767f8bb99de08d6536691785
SpyrexDE <malikfk100@gmail.com>
2020-12-03 21:12:40 +0100

    .

--- website/php/statsLoader.php
+++ website/php/statsLoader.php
@@ -1,5 +1,5 @@
 <?php
-echo getenv("sfDbName");
+var_dump(getenv("sfDbName"));
 
         //Mit Server verbinden und Datenbank auswaehlen
         $database = mysqli_connect("localhost", getenv("sfDbName"), getenv("sfDbPassword")) or die("Connection failed: " . mysqli_error($database));
