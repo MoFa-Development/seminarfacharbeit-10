d02ca6ef963485672439cee5d6546d6fc3dd420b
SpyrexDE <malikfk100@gmail.com>
2020-12-05 10:40:18 +0100

    .

--- website/stats.php
+++ website/stats.php
@@ -93,11 +93,11 @@ foreach($fr as $r)
         name: '$author',
         text: ['Autor: $author<br>CharRate: $charRate%<br>Duplikatswörter: $duplicateWords<br>InputLen: $inputLen<br>OutputLen: $outputLen'],
         mode: ['markers'],
-        marker: {
+        marker: [{
           size: [$duplicateWords*100],
           sizeref: 2,
           sizemode: 'area'
-        }
+        }]
       };
     ";
     $str .= "t".$rid."  , ";
