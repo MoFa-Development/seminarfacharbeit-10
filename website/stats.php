c4d0addabf2c1a4cb1dbdaad8b759677e3205ab1
Spyrex <malikfk100@gmail.com>
2020-12-07 12:39:41 +0100

    Filter-Dropdown hinzugefügt

--- website/stats.php
+++ website/stats.php
@@ -22,7 +22,32 @@
               <li><a href="about.php">About</a></li>
             </ul>
         </div>
-        
+            
+        <div class="dropdown is-active">
+          <div class="dropdown-trigger">
+            <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+              <span>Bitte auswählen</span>
+              <span class="icon is-small">
+                <i class="fas fa-angle-down" aria-hidden="true"></i>
+              </span>
+            </button>
+          </div>
+          <div class="dropdown-menu" id="dropdown-menu" role="menu">
+            <div class="dropdown-content">
+              <a href="?ord=title" class="dropdown-item <?php if($_GET["ord"] == "title") echo "is-active";?>">
+                Einzelne Werke
+              </a>
+              <a href="?ord=author" class="dropdown-item <?php if($_GET["ord"] == "author") echo "is-active";?>">
+                Autor
+              </a>
+              <a href="?ord=genre" class="dropdown-item <?php if($_GET["ord"] == "genre") echo "is-active";?>">
+                Genre
+              </a>
+            </div>
+          </div>
+        </div>
+
+
             <div id="plot"></div>
         
     </body>
@@ -31,7 +56,15 @@
 <?php
 
     include "php/statsLoader.php";
-    $ord = "genre";
+    
+    if(isset($_GET["ord"]))
+    {
+      $ord = $_GET["ord"];
+    }
+    else
+    {
+      return;
+    }
     $fr = loadStats($ord);
 
 ?>
