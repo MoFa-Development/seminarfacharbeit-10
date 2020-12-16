506331c82727f6e316dcbac7f00be1d4828accef
SpyrexDE <malikfk100@gmail.com>
2020-12-16 21:43:39 +0100

    style update

--- website/about.php
+++ website/about.php
@@ -27,7 +27,7 @@
               Diese Website nutzt das Git Repository unter <a href="https://github.com/Antricks/seminarfacharbeit-10">https://github.com/Antricks/seminarfacharbeit-10</a> der <strong>Version <?php exec("git rev-list --all --count 2>&1", $result); echo($result[0]); ?></strong>.
             </div>
           </div>
-        <div class="card" style="margin: 200px;  margin-top: 100px; padding: 50px;">
+        <div class="card" style="margin: auto;  margin-top: 100px; padding: 50px; max-width: 1000px;">
           <h1 class="title">Grundlagenforschung zur Komprimierungsrate von Texten verschiedener Art</h1>
           <hr>
           <p>Untersucht wird die Länge eines komprimierten Textes im Vergleich zum Ursprungstext. Dabei basiert die Komprimierung auf dem mehrfachen Auftreten von Zeichenketten. Dabei wird die Frage gestellt, in wie fern aus der Komprimierungsrate auf den Inhalt der Texte geschlossen werden kann.</p>
